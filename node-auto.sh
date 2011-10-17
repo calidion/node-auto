64b4520bddade4c3ec05e0eaf870d039661b7a09
Bai Lee <calidion@gmail.com>
2011-10-17 16:48:37 +0800

    enabled version and location customation default stable version v0.4.12

--- node-auto.sh
+++ node-auto.sh
@@ -1,8 +1,31 @@
+#!/usr/bin/bash
+
 # or git clone git://github.com/joyent/node.git if you want to checkout a stable tag
+
 git clone --depth 1 git://github.com/joyent/node.git 
 cd node
-git checkout v0.4.11 # optional.  Note that master is unstable.
-location=~/local/node
+
+if [ -z $1 ]; then
+  echo "checking default version v0.4.12"
+  git checkout v0.4.12 # optional.  Note that master is unstable.
+else
+  echo "checking custom version $1"
+fi
+
+if [ -z $2 ]; then
+  echo "using local default location"
+  location=~/local/node
+else
+  echo "using custom location"
+  if [ -d $2 ]; then
+    location=$2
+  else
+    echo "directory not found!"
+    echo "using default location instead"
+    location=~/local/node
+  fi
+fi
+
 ./configure --prefix=$location
 make -j2 # -j sets the number of jobs to run
 make install
@@ -11,5 +34,6 @@ export PATH=$PATH:$location/bin
 # ~/.bash_profile or ~/.bashrc on some systems
 echo "export NODE_PATH=$location:$location/lib/node_modules" >> ~/.profile 
 echo "export PATH=$PATH:$location/bin" >> ~/.profile 
+
 curl http://npmjs.org/install.sh | sh
 
