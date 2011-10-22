75a8678707e6a6545e7166388027e08ee15c4cd6
Bai Lee <calidion@gmail.com>
2011-10-22 05:05:39 -0400

    add debian auto install

---
+++ node-auto-debian.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+# or git clone git://github.com/joyent/node.git if you want to checkout a stable tag
+
+sudo apt-get install git python curl g++ libssl-dev make
+
+git clone --depth 1 git://github.com/joyent/node.git 
+cd node
+
+if [ -z $1 ]; then
+  echo "checking default version v0.4.12"
+  git checkout v0.4.12 # optional.  Note that master is unstable.
+else
+  echo "checking custom version $1"
+  git checkout $1 # optional.  Note that master is unstable.
+fi
+
+if [ -z $2 ]; then
+  echo "using local default location"
+  location=~/local/node
+else
+  echo "using custom location"
+  if [ -d $2 ]; then
+    location=$2
+  else
+    echo "directory not found!"
+    echo "using default location instead"
+    location=~/local/node
+  fi
+fi
+
+./configure --prefix=$location
+make -j2 # -j sets the number of jobs to run
+make install
+export NODE_PATH=$location:$location/lib/node_modules
+export PATH=$PATH:$location/bin
+# ~/.bash_profile or ~/.bashrc on some systems
+echo "export NODE_PATH=$location:$location/lib/node_modules" >> ~/.profile 
+echo "export PATH=$PATH:$location/bin" >> ~/.profile 
+
+curl http://npmjs.org/install.sh | sh
+
